f8d77b3f94d9bcf769831f1d00fcda995567435d
jd <jean.daniel.michaud@gmail.com>
2024-06-28 18:23:37 +0200

    add gists and remove the limit on the number of repos

--- mirror-github.sh
+++ mirror-github.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
-[ ! -d "/data" ] && echo "error: no volume mounted to /data" && exit 1
+#[ ! -d "/data" ] && echo "error: no volume mounted to /data" && exit 1
 
-cd /data
+#cd /data
 
-# Only 3 pages (max 300 repos)
-repos=`curl --silent -u jdmichaud:$gh_token https://api.github.com/user/repos?per_page=100 -q | grep clone_url | sed -e 's/[[:space:]]*"clone_url": "\([^"]*\)",/\1/g'i ; curl --silent -u jdmichaud:$gh_token https://api.github.com/user/repos?per_page=100\&page=2 -q | grep clone_url | sed -e 's/[[:space:]]*"clone_url": "\([^"]*\)",/\1/g' ; curl --silent -u jdmichaud:$gh_token https://api.github.com/user/repos?per_page=100\&page=3 -q | grep clone_url | sed -e 's/[[:space:]]*"clone_url": "\([^"]*\)",/\1/g'`
+cd /media/jedi/fastdisk/test/
 
-for repo in $repos
+page=1
+repos=()
+
+# Fetch repos list
+repos_per_page=("placeholder")
+while [ ${#repos_per_page[@]} -gt 0 ]
+do
+  echo Loading page $page
+  repos_per_page=($(curl --silent -u jdmichaud:$gh_token "https://api.github.com/user/repos?per_page=100&page=$page" -q | grep clone_url | sed -e 's/[[:space:]]*"clone_url": "\([^"]*\)",/\1/g'i))
+  echo Found ${#repos_per_page[@]} repos
+  repos+=(${repos_per_page[@]})
+  page=$(($page+1))
+done
+
+# Clone repos
+for repo in ${repos[@]}
 do
   repo_with_token=`echo $repo | sed -e "s#https://\(.*\)#https://$gh_token@\1#g"`
+  echo $repo_with_token
   git clone --mirror $repo_with_token
 done
 
@@ -22,3 +37,38 @@ do
   git remote update --prune
   popd > /dev/null
 done
+
+# Fetch gist list
+page=1
+gists=()
+gists_per_page=("placeholder")
+while [ ${#gists_per_page[@]} -gt 0 ]
+do
+  echo Loading page $page
+  gists_per_page=($(curl --silent -u jdmichaud:$gh_token "https://api.github.com/gists?per_page=100&page=$page" -q | grep git_pull_url | sed -e 's/[[:space:]]*"git_pull_url": "\([^"]*\)",/\1/g'i))
+  echo Found ${#gists_per_page[@]} gists
+  gists+=(${gists_per_page[@]})
+  page=$(($page+1))
+done
+
+# Clone gists in a sub folder
+mkdir -p gists
+cd gists
+for repo in ${gists[@]}
+do
+  repo_with_token=`echo $repo | sed -e "s#https://\(.*\)#https://$gh_token@\1#g"`
+  echo $repo_with_token
+  git clone --mirror $repo_with_token
+done
+
+# Update existing gists
+for folder in `ls`
+do
+  pushd . > /dev/null
+  cd $folder
+  echo updating $(pwd)
+  git remote update --prune
+  popd > /dev/null
+done
+cd -
+
